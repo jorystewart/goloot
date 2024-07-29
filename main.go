830fb73fa908ef2520ef3b593229a25b8bfcbe6b
jorystewart <jorymstewart@gmail.com>
2024-07-29 23:45:34 +0900

    EOD commit

--- main.go
+++ main.go
@@ -4,22 +4,26 @@ import (
   "database/sql"
   "fmt"
   _ "modernc.org/sqlite"
-  "net/http"
+  //"net/http"
 )
 
-func computersHandler(w http.ResponseWriter, r *http.Request) {
+/*func computersHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Computers")
-}
+}*/
 
 func main() {
-  http.HandleFunc("/computers", computersHandler)
-  http.ListenAndServe(":5050", nil)
-}
-
-func connect_db(name string) *sql.DB {
-  db, err := sql.Open(name, "jdbc:sqlite:D:\\source\\repos\\jorystewart\\goloot\\data\\roster.sqlite")
+  db, err := sql.Open("sqlite", "jdbc:sqlite:D:\\source\\repos\\jorystewart\\goloot\\data\\roster.sqlite")
   if err != nil {
     fmt.Sprintln("Unable to connect to database")
+    return
   }
-  return db
+  defer db.Close()
+
+  queryResult, _ := db.Query("SELECT * FROM roster WHERE name = 'Anthalon'")
+  for queryResult.Next() {
+    fmt.Println(queryResult.Scan())
+  }
+
+  //http.HandleFunc("/computers", computersHandler)
+  //http.ListenAndServe(":5050", nil)
 }
